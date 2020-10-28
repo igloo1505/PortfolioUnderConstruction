3251db3a425180edb62d4afa6599e60ca73c544d
Andrew <iglooworks1505@gmail.com>
2020-10-27 22:01:02 -0500

    added google analytics and fixed some broken links

--- client/src/App.js
+++ client/src/App.js
@@ -7,14 +7,53 @@ import ReactGa from 'react-ga'
 import AboutMe from "./pages/AboutMe";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
-
+import ttiPolyfill from 'tti-polyfill'
 import "./index.css";
 
 function App() {
   let sideNav;
   useEffect(() => {
-    ReactGa.initialize("UA-181494074-1")
+    ReactGa.initialize("UA-181494074-1", {
+      gaOptions: { siteSpeedSampleRate: 100 }
+    })
+    ReactGa.addTrackers([
+      { 
+        trackingId: 'G-YJK7R2Y1TL',
+        gaOptions: { siteSpeedSampleRate: 100, name: 'GA tracker' }
+      }
+    ])
   }, [])
+
+  const handlePerformance = list => {
+    list.getEntries().forEach(entry => {
+      ReactGa.timing({
+        category: "Load Performance",
+        variable: 'Server Latency',
+        value: entry.responseStart - entry.requestStart 
+      })
+      ReactGa.timing({ 
+        category: "Load Performance",
+        variable: 'Download Time',
+        value: entry.responseEnd - entry.responseStart
+      })
+      ReactGa.timing({
+        category: "Load Performance",
+        variable: 'Total App Load Time',
+        value: entry.responseEnd - entry.requestStart
+      })
+  })
+}
+ttiPolyfill.getFirstConsistentlyInteractive().then((tti) => {
+  ReactGa.timing({
+    category: "Load Performance",
+    variable: 'Time to Interactive',
+    value: tti 
+  })
+});
+var observer = new PerformanceObserver(handlePerformance);
+observer.observe({entryTypes: ['navigation'] })
+
+
 
   useEffect(() => {
     M.AutoInit();
@@ -22,12 +61,11 @@ function App() {
     var instances = M.Sidenav.init(elems);
     sideNav = instances[0];
     console.log(sideNav);
-  });
+  }, []);
   const [curPath, setPath] = useState("/");
   useEffect(() => {
     setPath(window.location.pathname);
-    console.log(curPath);
-  });
+  }, []);
 
   return (
     <div className="App">
